48c8ca8ee85d1563e70bac825313e14dbfb8502d
NatumeMituan <NatumeMituan@gmail.com>
2025-12-06 10:19:00 +0800

    Implement type

--- src/main.cs
+++ src/main.cs
@@ -1,31 +1,60 @@
 class Program
 {
-    static void Main()
+    private const string Echo = "echo";
+    private const string Exit = "exit";
+    private const string Type = "type";
+
+    private static readonly HashSet<string> BuiltInCommands = [Echo, Exit, Type];
+
+    public static void Main()
     {
         while (true)
         {
             Console.Write("$ ");
-
             var command = Console.ReadLine();
-            if(string.IsNullOrEmpty(command))
+            if (string.IsNullOrEmpty(command))
             {
                 continue;
             }
 
-            if(command == "exit")
+            if (command == Exit)
             {
                 break;
             }
 
             var inputs = command.Split(' ');
-            if(inputs[0] == "echo")
+            switch (inputs[0])
             {
-                var output = string.Join(' ', inputs[1..]);
-                Console.WriteLine(output);
+                case Echo:
+                    ExecuateEcho(inputs[1..]);
+                    break;
+                case Type:
+                    ExecuteType(inputs[1..]);
+                    break;
+                default:
+                    Console.WriteLine($"{command}: command not found");
+                    break;
+            }
+        }
+    }
+
+    private static void ExecuateEcho(string[] inputs)
+    {
+        var output = string.Join(' ', inputs);
+        Console.WriteLine(output);
+    }
+
+    private static void ExecuteType(string[] inputs)
+    {
+        foreach (var input in inputs)
+        {
+            if (BuiltInCommands.Contains(input))
+            {
+                Console.WriteLine($"{input} is a shell builtin");
             }
             else
             {
-                Console.WriteLine($"{command}: command not found");
+                Console.WriteLine($"{input}: not found");
             }
         }
     }
