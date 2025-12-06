6b5dea7d6edb1cb9b2d91f7c939aad71895927ef
NatumeMituan <NatumeMituan@gmail.com>
2025-12-06 10:01:16 +0800

    Implement echo

--- src/main.cs
+++ src/main.cs
@@ -7,12 +7,26 @@ class Program
             Console.Write("$ ");
 
             var command = Console.ReadLine();
+            if(string.IsNullOrEmpty(command))
+            {
+                continue;
+            }
+
             if(command == "exit")
             {
                 break;
             }
 
-            Console.WriteLine($"{command}: command not found");
+            var inputs = command.Split(' ');
+            if(inputs[0] == "echo")
+            {
+                var output = string.Join(' ', inputs[1..]);
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine($"{command}: command not found");
+            }
         }
     }
 }
