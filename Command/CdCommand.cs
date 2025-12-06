174ae4782c7e5c7afc082e15537ec4f431791ec7
NatumeMituan <NatumeMituan@gmail.com>
2025-12-06 16:04:49 +0800

    Implement cd for absolute path

---
+++ Command/CdCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace Codecrafters.Shell.Command;
+
+internal class CdCommand(string[] args) : ICommand
+{
+    public void Execute()
+    {
+        if (args.Length > 0)
+        {
+            var path = args[0];
+            if (path.StartsWith('/') && Directory.Exists(path))
+            {
+                Environment.CurrentDirectory = path;
+            }
+            else
+            {
+                Console.WriteLine($"cd: {path}: No such file or directory");
+            }
+        }
+    }
+}
--- Constants.cs
+++ Constants.cs
@@ -6,6 +6,7 @@ internal static class Constants
 {
     public static readonly Dictionary<string, Func<string, string[], ICommand>> BuiltInCommands = new()
     {
+        ["cd"] = (command, args) => new CdCommand(args),
         ["echo"] = (command, args) => new EchoCommand(args),
         ["exit"] = (command, args) => new ExitCommand(),
         ["pwd"] = (command, args) => new PwdCommand(),
