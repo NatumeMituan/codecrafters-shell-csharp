cee8e7969b7e2dd2470fbca6a23e02c4af053e08
NatumeMituan <NatumeMituan@gmail.com>
2025-12-17 21:30:21 +0800

    Refactor code

--- Completion/LineReader.cs
+++ Completion/LineReader.cs
@@ -4,74 +4,109 @@ namespace Codecrafters.Shell.Completion;
 
 internal static class LineReader
 {
+    private const char BellCharacter = '\x07';
+    private static readonly StringBuilder Line = new();
+    private static readonly Context ctx = new();
+
     public static string ReadLine(string prompt)
     {
-        const char BellCharacter = '\x07';
+        Line.Clear();
+        ctx.WasMultiCompletions = false;
+
         Console.Write(prompt);
-        var line = new StringBuilder();
-        bool wasMultiCompletions = false;
         ConsoleKeyInfo keyInfo;
 
         do
         {
-            bool isMultiCompletions = false;
+            ctx.IsMultiCompletions = false;
             keyInfo = Console.ReadKey(intercept: true);
-            if (keyInfo.Key == ConsoleKey.Backspace && line.Length > 0)
+
+            if (keyInfo.Key == ConsoleKey.Backspace && Line.Length > 0)
             {
-                line.Length--;
-                Console.Write("\b \b");
+                HandleBackspace();
             }
             else if (keyInfo.Key == ConsoleKey.Tab)
             {
-                var currentLine = line.ToString();
-                var completions = CompletionProvider.GetCompletions(currentLine);
-                var longestCommonPrefix = GetLongestCommonPrefix(currentLine.Length, completions);
-
-                if (completions.Count == 0)
-                {
-                    Console.Write(BellCharacter);
-                }
-                else if (completions.Count == 1)
-                {
-                    var completion = $"{completions[0][currentLine.Length..]} ";
-                    Write(line, completion);
-                }
-                else
-                {
-                    if (wasMultiCompletions)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine(string.Join("  ", completions.OrderBy(c => c)));
-                        Console.Write($"{prompt}{line}");
-                    }
-                    else if (longestCommonPrefix?.Length > currentLine.Length)
-                    {
-                        var completion = longestCommonPrefix[currentLine.Length..];
-                        Write(line, completion);
-                    }
-                    else
-                    {
-                        isMultiCompletions = true;
-                        Console.Write(BellCharacter);
-                    }
-                }
+                HandleTab(prompt);
             }
             else if (!char.IsControl(keyInfo.KeyChar))
             {
-                line.Append(keyInfo.KeyChar);
-                Console.Write(keyInfo.KeyChar);
+                HandleCharacter(keyInfo.KeyChar);
             }
 
-            wasMultiCompletions = isMultiCompletions;
+            ctx.WasMultiCompletions = ctx.IsMultiCompletions;
         } while (keyInfo.Key != ConsoleKey.Enter);
 
         Console.WriteLine();
-        return line.ToString();
+        return Line.ToString();
     }
 
-    private static void Write(StringBuilder line, string value)
+    private static void HandleBackspace()
     {
-        line.Append(value);
+        if (Line.Length > 0)
+        {
+            Line.Length--;
+            Console.Write("\b \b");
+        }
+    }
+
+    private static void HandleTab(string prompt)
+    {
+        var currentLine = Line.ToString();
+        var completions = CompletionProvider.GetCompletions(currentLine);
+
+        if (completions.Count == 0)
+        {
+            Console.Write(BellCharacter);
+        }
+        else if (completions.Count == 1)
+        {
+            var completion = $"{completions[0][currentLine.Length..]} ";
+            Write(completion);
+        }
+        else
+        {
+            HandleMultipleCompletions(prompt, currentLine, completions);
+        }
+    }
+
+    private static void HandleMultipleCompletions(
+        string prompt,
+        string currentLine,
+        List<string> completions)
+    {
+
+        if (ctx.WasMultiCompletions)
+        {
+            Console.WriteLine();
+            Console.WriteLine(string.Join("  ", completions.OrderBy(c => c)));
+            Console.Write($"{prompt}{Line}");
+        }
+        else
+        {
+            var longestCommonPrefix = GetLongestCommonPrefix(currentLine.Length, completions);
+            if (longestCommonPrefix?.Length > currentLine.Length)
+            {
+                var completion = longestCommonPrefix[currentLine.Length..];
+                Write(completion);
+            }
+            else
+            {
+                ctx.IsMultiCompletions = true;
+                Console.Write(BellCharacter);
+            }
+        }
+    }
+
+    private static void HandleCharacter(char c)
+    {
+        Line.Append(c);
+        Console.Write(c);
+    }
+
+    private static void Write(string value)
+    {
+        Line.Append(value);
         Console.Write(value);
     }
 
@@ -98,5 +133,11 @@ internal static class LineReader
         }
 
         return completions[0][..l];
+    }
+
+    private sealed class Context
+    {
+        public bool WasMultiCompletions { get; set; }
+        public bool IsMultiCompletions { get; set; }
     }
 }
